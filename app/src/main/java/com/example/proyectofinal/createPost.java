8441c5d5e67f5d900a635f2d6e46b3d8aeb58975
Juan Carlos Solorzano Castanedo <jcarlossolc@gmail.com>
2021-11-05 13:02:20 -0600

    Pantalla de añadir post

---
+++ app/src/main/java/com/example/proyectofinal/createPost.java
@@ -0,0 +1,16 @@
+package com.example.proyectofinal;
+
+import static com.example.proyectofinal.R.layout.activity_create_post;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_create_post);
+    }
+}
